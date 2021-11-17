6187a950b6ed57843033174e118b78f1a59cf97e
Julien Herou <julien.herou@gmail.com>
2021-11-17 16:05:18 +0100

    refresh number au drag

--- script.js
+++ script.js
@@ -38,43 +38,60 @@ precedent.forEach(element => {
 /*
 MEASURE
 */
-// Variable regle
+// Variables
 const measureElt1 = document.querySelector('.measure1');
 const measureElt2 = document.querySelector('.measure2');
-const lineElt = document.querySelectorAll('.contain-line');
+const lineElt1 = document.querySelector('.line1');
+const lineElt2 = document.querySelector('.line2');
 const slide2Elt = document.querySelector('.slide2');
 const slide4Elt = document.querySelector('.slide4');
+const numberElt1 = document.querySelector('.number1');
+const numberElt2 = document.querySelector('.number2');
+const resultElt = document.querySelector('.result');
+
 
 // Ecouteur de click sur les lignes (slide 2 et 4)
-lineElt.forEach(element => {
-    element.addEventListener('mousedown', mousedown);
-});
+lineElt1.addEventListener('mousedown', mousedown1);
+lineElt2.addEventListener('mousedown', mousedown2);
 
 // Fonction déclenchée au click
-function mousedown(e) {
+function mousedown1(e) {
     let prevY1 = e.clientY;
-    let prevY2 = e.clientY;
-    slide2Elt.addEventListener('mousemove', mousemove);
-    slide2Elt.addEventListener('mouseup', mouseup);
-    slide4Elt.addEventListener('mousemove', mousemove);
-    slide4Elt.addEventListener('mouseup', mouseup);
+    slide2Elt.addEventListener('mousemove', mousemove1);
+    slide2Elt.addEventListener('mouseup', mouseup1);
 
-    function mousemove(e) {
+    function mousemove1(e) {
         const rect1 = measureElt1.getBoundingClientRect();
         measureElt1.style.height = rect1.height - (prevY1 - e.clientY) + "px";
-        const rect2 = measureElt2.getBoundingClientRect();
-        measureElt2.style.height = rect2.height - (prevY2 - e.clientY) + "px";
         prevY1 = e.clientY;
-        prevY2 = e.clientY;
-        
-        console.log(measureElt1.offsetHeight);
+        let wristSize1Px = measureElt1.offsetHeight
+        let wristSize1Cm = (wristSize1Px * 0.0264583333).toFixed(2);
+        console.log(wristSize1Px);
+        console.log(wristSize1Cm);
+        numberElt1.innerHTML = wristSize1Cm;
     }
 
-    function mouseup() {
-        slide2Elt.removeEventListener('mousemove', mousemove);
-        slide2Elt.removeEventListener('mouseup', mouseup);
-        slide4Elt.removeEventListener('mousemove', mousemove);
-        slide4Elt.removeEventListener('mouseup', mouseup);
+    function mouseup1() {
+        slide2Elt.removeEventListener('mousemove', mousemove1);
+        slide2Elt.removeEventListener('mouseup', mouseup1);
+    }
+    
+}
+
+function mousedown2(e) {
+    let prevY2 = e.clientY;
+    slide4Elt.addEventListener('mousemove', mousemove2);
+    slide4Elt.addEventListener('mouseup', mouseup2);
+
+    function mousemove2(e) {
+        const rect2 = measureElt2.getBoundingClientRect();
+        measureElt2.style.height = rect2.height - (prevY2 - e.clientY) + "px";
+        prevY2 = e.clientY;
+    }
+
+    function mouseup2() {
+        slide4Elt.removeEventListener('mousemove', mousemove2);
+        slide4Elt.removeEventListener('mouseup', mouseup2);
     }
     
 }
