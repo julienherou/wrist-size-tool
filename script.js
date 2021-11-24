e801628ebfda15d28ef0d180ce311b273207ad08
Julien Herou <julien.herou@gmail.com>
2021-11-24 19:05:39 +0100

    ajout du touch sur app

--- script.js
+++ script.js
@@ -21,28 +21,22 @@ let calcul;
 let wristSizePx;
 let wristSizeCm;
 
+
 // Fonction avec element à mesurer (source) et zone de texte à remplacer (destination)
-function measureElt(source, destination) {
-    wristSizePx = source.offsetHeight
-    wristSizeCm = (wristSizePx * 0.0264583333).toFixed(2);
+function measureElt(source, destination, ligne) {
+    wristSizePx = source.offsetHeight - (ligne.offsetHeight / 2)
+    // wristSizeCm = (wristSizePx * 0.0264583333).toFixed(2);
+    wristSizeCm = (wristSizePx * 0.0264583333).toFixed(2).replace(/\./g, '\,');
     destination.innerHTML = wristSizeCm;
+    // var res = str.replace(/\./g, ' ');
 }
-// Formule (rentrer les mesures en px)
+
+// Formule (rentrer les mesures en px) >> calcul final
 function formule(mesure1, mesure2) {
-    calcul = (((mesure1 + mesure2) * 1.79) * 0.0264583333).toFixed(1);
+    calcul = (((mesure1 + mesure2) * 1.79) * 0.0264583333).toFixed(1).replace(/\./g, '\,');
     return calcul;
 }
 
-
-function measureEltTest(source, destination, ligne) {
-    wristSizePx = source.offsetHeight - (ligne.offsetHeight / 2)
-    console.log('wristSizePx :' + wristSizePx);
-    wristSizeCm = (wristSizePx * 0.0264583333).toFixed(2);
-    destination.innerHTML = wristSizeCm;
-}
-
-
-// FONCTIONS
 
 // fonction page/slide suivante
 function pageSuivante(){
@@ -69,8 +63,8 @@ function pageSuivante(){
     pages[count].classList.add('active');
 
 
-    measureElt(measureElt1, numberElt1);
-    measureElt(measureElt2, numberElt2);
+    measureElt(measureElt1, numberElt1, lineElt1);
+    measureElt(measureElt2, numberElt2, lineElt2);
     // Affichage de la mesure au chargement de la page
     // let wristSize1Px = measureElt1.offsetHeight
     // let wristSize1Cm = (wristSize1Px * 0.0264583333).toFixed(2);
@@ -97,8 +91,8 @@ function pagePrecedente(){
         count = nbPages - 1;
     }
     pages[count].classList.add('active');
-    measureElt(measureElt1, numberElt1);
-    measureElt(measureElt2, numberElt2);
+    measureElt(measureElt1, numberElt1, lineElt1);
+    measureElt(measureElt2, numberElt2, lineElt2);
 }
 
 precedent.forEach(element => {
@@ -113,6 +107,8 @@ lineElt1.addEventListener('mousedown', mousedown1);
 lineElt2.addEventListener('mousedown', mousedown2);
 
 
+
+
 // SLIDE 2
 // Fonction déclenchée au click
 function mousedown1(e) {
@@ -125,13 +121,11 @@ function mousedown1(e) {
         const rect1 = measureElt1.getBoundingClientRect();
         measureElt1.style.height = rect1.height - (prevY1 - e.clientY) + "px";
         prevY1 = e.clientY;
-
         // let wristSize1Px = measureElt1.offsetHeight
         // let wristSize1Cm = (wristSize1Px * 0.0264583333).toFixed(2);
         // resulats[0] = wristSize1Cm;
         // numberElt1.innerHTML = wristSize1Cm;
-        measureElt(measureElt1, numberElt1);
-        measureEltTest(measureElt1, numberElt1, lineElt1);
+        measureElt(measureElt1, numberElt1, lineElt1);
     }
 
     function mouseup1() {
@@ -145,7 +139,6 @@ function mousedown1(e) {
     // }
 
 }
-
 
 
 
@@ -163,7 +156,7 @@ function mousedown2(e) {
         // let wristSize2Px = measureElt2.offsetHeight
         // let wristSize2Cm = (wristSize2Px * 0.0264583333).toFixed(2);
         // numberElt2.innerHTML = wristSize2Cm;
-        measureElt(measureElt2, numberElt2);
+        measureElt(measureElt2, numberElt2, lineElt2);
     }
 
     function mouseup2() {
@@ -176,6 +169,46 @@ function mousedown2(e) {
     
 }
 
+
+// POUR MOBILE
+
+// Ecouteur de touch sur les lignes (slide 2 et 4)
+lineElt1.addEventListener('touchstart', touchstart1);
+lineElt2.addEventListener('touchstart', touchstart2);
+
+// SLIDE 2
+// Fonction déclenchée au touch
+function touchstart1(e) {
+    let prevY3 = e.targetTouches[0].clientY;
+    slide2Elt.addEventListener('touchmove', touchmove1);
+    slide2Elt.addEventListener('touchend', touchend1);
+    function touchmove1(e) {
+        const rect3 = measureElt1.getBoundingClientRect();
+        measureElt1.style.height = rect3.height - (prevY3 - e.targetTouches[0].clientY) + "px";
+        prevY3 = e.targetTouches[0].clientY;
+        measureElt(measureElt1, numberElt1, lineElt1);
+    }
+    function touchend1() {
+        slide2Elt.removeEventListener('touchmove', touchmove1);
+        slide2Elt.removeEventListener('touchend', touchend1);
+    }
+}
+// SLIDE 4
+function touchstart2(e) {
+    let prevY4 = e.targetTouches[0].clientY;
+    slide4Elt.addEventListener('touchmove', touchmove2);
+    slide4Elt.addEventListener('touchend', touchend2);
+    function touchmove2(e) {
+        const rect4 = measureElt2.getBoundingClientRect();
+        measureElt2.style.height = rect4.height - (prevY4 - e.targetTouches[0].clientY) + "px";
+        prevY4 = e.targetTouches[0].clientY;
+        measureElt(measureElt2, numberElt2, lineElt2);
+    }
+    function touchend2() {
+        slide4Elt.removeEventListener('touchmove', touchmove2);
+        slide4Elt.removeEventListener('touchend', touchend2);
+    }
+}
 
 
 
