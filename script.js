22317d12e250a824239740535648c483bf44c5c9
Julien Herou <julien.herou@gmail.com>
2022-01-02 00:22:33 +0100

    modif calcul, commit avant clean code

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function getResolution() {
 // getResolution();
 
 
-
 // Permet d'ajuster la hauteur sur mobile en 100vh
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
@@ -42,22 +41,31 @@ const lineElt1 = document.querySelector('.line1');
 const lineElt2 = document.querySelector('.line2');
 const slide2Elt = document.querySelector('.slide2');
 const slide4Elt = document.querySelector('.slide4');
+// Selecteurs d'affichage mesure 1 & 2
 const numberElt1 = document.querySelector('.number1');
+const symbElt1 = document.querySelector('.symb-unit1');
 const numberElt2 = document.querySelector('.number2');
+const symbElt2 = document.querySelector('.symb-unit2');
 const resultElt = document.querySelector('.result');
-let resulats = [];
+const symbElt3 = document.querySelector('.symb-unit3');
+let resultats = [];
 let calcul;
 const unitElt = document.querySelectorAll('.unit');
+
+
 
 // Conversion pixel >> cm et affichage au chargement de la page
 let wristSizePx;
 let wristSizeCm;
 let convert;
 
-// Conversion 96dpi en cm :
+// Conversion 96dpi px en cm :
 convert = 0.0264583333;
-// Conversion 144dpi en cm :
+// en test
+let convertCm = 0.0264583333;
+// Conversion 144dpi px en cm :
 // convert = 0.017638889;
+
 
 // Fonction avec element à mesurer (source) et zone de texte à remplacer (destination)
 function measureElt(source, destination, ligne) {
@@ -70,18 +78,12 @@ function measureElt(source, destination, ligne) {
 }
 
 
-// Fonctions de calcul
-// let wristSizePxOne;
-function validMeasureOne(){
-    let wristSizePxOne = measureElt1.offsetHeight - (lineElt1.offsetHeight / 2);
-    resulats[0] = wristSizePxOne;
-    console.log('mesure 1 : ' + resulats[0] + 'px');
-}
-// let wristSizePxTwo;
-function validMeasureTwo(){
-    let wristSizePxTwo = measureElt2.offsetHeight - (lineElt2.offsetHeight / 2);
-    resulats[1] = wristSizePxTwo;
-    console.log('mesure 2 : ' + resulats[1] + 'px');
+// en test, semble marcher
+function showMeasureInCm(source, destination, ligne, unite) {
+    wristSizePx = source.offsetHeight - (ligne.offsetHeight / 2);
+    wristSizeCm = (wristSizePx * convertCm).toFixed(2).replace(/\./g, '\,');
+    destination.innerHTML = wristSizeCm;
+    unite.innerHTML = ' cm';
 }
 
 // Formule (rentrer les mesures en px) >> calcul final
@@ -90,6 +92,12 @@ function formule(mesure1, mesure2) {
     return calcul;
 }
 
+function showResultInCm(mesure1, mesure2, destination, unite){
+    calcul = (((mesure1 + mesure2) * 1.79) * convert).toFixed(1).replace(/\./g, '\,');
+    destination.innerHTML = calcul;
+    unite.innerHTML = ' cm';
+    console.log('resulat : ' + calcul + 'cm');
+}
 
 
 
@@ -106,31 +114,32 @@ function changeUnit(){
     if (this.classList.contains('unit-cm')){
         console.log('jesuisencm');
     }
-
-
 };
 
 
 
 
-
-
+// On écoute sur chaque slide le click sur "Confirmer"
+suivant.forEach(element => {
+    element.addEventListener('click', pageSuivante);
+});
 // fonction page/slide suivante
 function pageSuivante(){
-
     // console.log(count);
     // Pour déclencher le calcul final
+    // Si on valide la 1ere mesure
     if(count == 1) {
-        // resulats[0] = measureElt1.offsetHeight;
-        validMeasureOne();
+        resultats[0] = measureElt1.offsetHeight - (lineElt1.offsetHeight / 2);
+        console.log('mesure 1 : ' + resultats[0] + 'px');
     }
+    // Si on valide la 2eme mesure
     if(count == 3) {
-        // resulats[1] = measureElt2.offsetHeight;
-        validMeasureTwo();
-        formule(resulats[0], resulats[1]);
-        resultElt.innerHTML = calcul;
+        resultats[1] = measureElt2.offsetHeight - (lineElt2.offsetHeight / 2);
+        console.log('mesure 2 : ' + resultats[1] + 'px');
+        // formule(resultats[0], resultats[1]);
+        // resultElt.innerHTML = calcul;
+        showResultInCm(resultats[0], resultats[1], resultElt, symbElt3)
     }
-
     pages[count].classList.remove('active');
     if(count < nbPages - 1){
         count++;
@@ -138,18 +147,18 @@ function pageSuivante(){
         count = 0;
     }
     pages[count].classList.add('active');
+    // measureElt(measureElt1, numberElt1, lineElt1);
+    showMeasureInCm(measureElt1, numberElt1, lineElt1, symbElt1);
+    showMeasureInCm(measureElt2, numberElt2, lineElt2, symbElt2);
+    // measureElt(measureElt2, numberElt2, lineElt2);
+} // Fin de fonction pageSuivante
 
-    measureElt(measureElt1, numberElt1, lineElt1);
-    measureElt(measureElt2, numberElt2, lineElt2);
-
-}
 
 
-// On écoute le click sur "Confirm"
-suivant.forEach(element => {
-    element.addEventListener('click', pageSuivante);
+// On écoute sur chaque slide le click sur "Retour"
+precedent.forEach(element => {
+    element.addEventListener('click', pagePrecedente);
 });
-
 // fonction page/slide précédente
 function pagePrecedente(){
     pages[count].classList.remove('active');
@@ -159,81 +168,64 @@ function pagePrecedente(){
         count = nbPages - 1;
     }
     pages[count].classList.add('active');
-    measureElt(measureElt1, numberElt1, lineElt1);
-    measureElt(measureElt2, numberElt2, lineElt2);
+    // measureElt(measureElt1, numberElt1, lineElt1);
+    showMeasureInCm(measureElt1, numberElt1, lineElt1, symbElt1);
+    showMeasureInCm(measureElt2, numberElt2, lineElt2, symbElt2);
+    // measureElt(measureElt2, numberElt2, lineElt2);
 }
 
-precedent.forEach(element => {
-    element.addEventListener('click', pagePrecedente);
-});
 
 
 
-
+// POUR DESKTOP
 // Ecouteur de click sur les lignes (slide 2 et 4)
 lineElt1.addEventListener('mousedown', mousedown1);
 lineElt2.addEventListener('mousedown', mousedown2);
 
-
-
 // SLIDE 2
-// Fonction déclenchée au click
+// Fonction déclenchée au click sur la reglette
 function mousedown1(e) {
     let prevY1 = e.clientY;
     slide2Elt.addEventListener('mousemove', mousemove1);
     slide2Elt.addEventListener('mouseup', mouseup1);
-
     function mousemove1(e) {
         const rect1 = measureElt1.getBoundingClientRect();
         measureElt1.style.height = rect1.height - (prevY1 - e.clientY) + "px";
-
         prevY1 = e.clientY;
-
-        measureElt(measureElt1, numberElt1, lineElt1);
-
+        // measureElt(measureElt1, numberElt1, lineElt1);
+        showMeasureInCm(measureElt1, numberElt1, lineElt1, symbElt1);
     }
-
     function mouseup1() {
         slide2Elt.removeEventListener('mousemove', mousemove1);
         slide2Elt.removeEventListener('mouseup', mouseup1);
     }
-    
-
 }
-
-
-
 // SLIDE 4
-// Fonction déclenchée au click
+// Fonction déclenchée au click sur la reglette
 function mousedown2(e) {
     let prevY2 = e.clientY;
     slide4Elt.addEventListener('mousemove', mousemove2);
     slide4Elt.addEventListener('mouseup', mouseup2);
-
     function mousemove2(e) {
         const rect2 = measureElt2.getBoundingClientRect();
         measureElt2.style.height = rect2.height - (prevY2 - e.clientY) + "px";
         prevY2 = e.clientY;
-        measureElt(measureElt2, numberElt2, lineElt2);
+        // measureElt(measureElt2, numberElt2, lineElt2);
+        showMeasureInCm(measureElt2, numberElt2, lineElt2, symbElt2);
     }
-
     function mouseup2() {
         slide4Elt.removeEventListener('mousemove', mousemove2);
         slide4Elt.removeEventListener('mouseup', mouseup2);
     }
-
-    
 }
 
 
 // POUR MOBILE
-
 // Ecouteur de touch sur les lignes (slide 2 et 4)
 lineElt1.addEventListener('touchstart', touchstart1);
 lineElt2.addEventListener('touchstart', touchstart2);
-
 // SLIDE 2
-// Fonction déclenchée au touch
+// Fonction déclenchée au touch sur la reglette
 function touchstart1(e) {
     let prevY3 = e.targetTouches[0].clientY;
     slide2Elt.addEventListener('touchmove', touchmove1);
@@ -242,7 +234,8 @@ function touchstart1(e) {
         const rect3 = measureElt1.getBoundingClientRect();
         measureElt1.style.height = rect3.height - (prevY3 - e.targetTouches[0].clientY) + "px";
         prevY3 = e.targetTouches[0].clientY;
-        measureElt(measureElt1, numberElt1, lineElt1);
+        // measureElt(measureElt1, numberElt1, lineElt1);
+        showMeasureInCm(measureElt1, numberElt1, lineElt1, symbElt1);
     }
     function touchend1() {
         slide2Elt.removeEventListener('touchmove', touchmove1);
@@ -258,7 +251,8 @@ function touchstart2(e) {
         const rect4 = measureElt2.getBoundingClientRect();
         measureElt2.style.height = rect4.height - (prevY4 - e.targetTouches[0].clientY) + "px";
         prevY4 = e.targetTouches[0].clientY;
-        measureElt(measureElt2, numberElt2, lineElt2);
+        // measureElt(measureElt2, numberElt2, lineElt2);
+        showMeasureInCm(measureElt2, numberElt2, lineElt2, symbElt2);
     }
     function touchend2() {
         slide4Elt.removeEventListener('touchmove', touchmove2);
